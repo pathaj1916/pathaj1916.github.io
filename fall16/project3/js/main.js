e657768adef591b2c65d4d022adca04460527009
pathaj1916 <PatriciaH@Patricias-MacBook-Pro.local>
2016-11-18 08:09:55 -0800

    Progress Project 3
    Digital Comps and 4 coded pages

---
+++ fall16/project3/js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+	$('#nav-menu-btn').on('click', function () {
+			var $menuBtn = $(this),
+				menuExpanded = $menuBtn.attr('aria-expanded'),
+				$navMenu = $('.main-nav');
+
+			if (menuExpanded === 'true') {
+					$menuBtn.attr('aria-expanded','false').removeClass('expanded');
+					$navMenu.removeClass('expanded');
+			} else {
+					$menuBtn.attr('aria-expanded','true').addClass('expanded');
+					$navMenu.addClass('expanded');
+			}
+	}); 
+
+	$('.squarecircle-btn').on('click', function (e) {
+		e.preventDefault();
+		$(this).parent().siblings('.copyblock').toggleClass('hidden');
+	});
+});
